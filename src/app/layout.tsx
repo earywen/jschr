c9d98288b38b1e9cde486823de61e31f208be5cd
Laurent <brigaud.laurent@gmail.com>
2026-01-14 23:29:47 +0100

    chore(metadata): update site title to Jet Set Club - Apply

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "JSC HR",
+  title: "Jet Set Club - Apply",
   description: "Recruitment platform for JSC",
 };
 
